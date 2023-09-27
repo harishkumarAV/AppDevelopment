88c702962d71718c892441abf4a7f62c6ccf8291
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2023-09-27 13:27:53 +0530

    Add files via upload

---
+++ Day10/src/Component/Asserts/PageNotAvailable/PageNotAvailable.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './PageNotAvailable.css';
+
+function PageNotAvailable() {
+  return (
+    <div className="page-not-available">
+      <p>Page not available. </p>
+    </div>
+  );
+}
+
+export default PageNotAvailable;
