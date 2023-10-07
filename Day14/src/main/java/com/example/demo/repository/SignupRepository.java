c2f195149f3375cb019f2097b382ea2c981aba4c
Harish Kumar A V <110531457+harishkumarAV@users.noreply.github.com>
2023-10-07 11:43:29 +0530

    Delete Day14 directory

--- Day14/src/main/java/com/example/demo/repository/SignupRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.entity.Signup;
-public interface SignupRepository extends JpaRepository<Signup,String>{
-
-}
-
-
